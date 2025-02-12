47458c03df3eb781e64ce1682577687fd85ae4db
chadmanningfiddle <Chadmanning@gmail.com>
2025-02-12 10:22:41 +0000

    crunchy corks

--- server/app.cjs
+++ server/app.cjs
@@ -1,6 +1,6 @@
-import express from 'express';
-import session from 'express-session';
-import cors from 'cors';
+const express = require('express');
+const session = require('express-session');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
